332e8c9d9cff41f0cdda50443efcfeb48ee48170
Alberto Sonnino <alberto@asonnino2.cs.ucl.ac.uk>
2017-10-16 17:41:52 +0100

    [AWS] Protocol implementation.

---
+++ aggregate_signature/aws/signer.py
@@ -0,0 +1,62 @@
+""" """
+from aggregate_signature.lib import setup, keygen, sign
+
+from json  import loads, dumps
+from flask import Flask, request
+
+
+
+# ==================================================
+# Signer class
+# ==================================================
+class Signer():
+	# ----------------------------------------------
+	# init
+	# ----------------------------------------------
+	def __init__(self):
+		self.params = setup()
+		(self.sk, self.vk) = keygen(self.params)
+
+	# ----------------------------------------------
+	# sign
+	# ----------------------------------------------
+	def sign(self, requestData):
+		m = loads(requestData)
+		return sign(self.params, self.sk, m)
+
+
+
+
+# ==================================================
+# Webapp
+# ==================================================
+app = Flask(__name__)
+app.signer = Signer()
+
+
+# index
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return dumps({"status": "OK", "message": "Hello, world!"})
+
+
+# issue a signature 
+@app.route("/sign", methods=["GET", "POST"])
+def sign():
+    if request.method == "POST":
+        try:
+        	returns = app.signer.sign(request.data)
+        	return dumps({"status" : "OK", "returns" : returns})
+        except Exception as e:
+        	return dumps({"status"  : "ERROR", "message" : e.args})
+    else:
+    	return dumps({"status": "ERROR", "message":"Use POST method."})
+
+
+
+# ==================================================
+# entry point
+# ==================================================
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port="5001") 
+
---
+++ aggregate_signature/aws/user.py
@@ -0,0 +1,48 @@
+""" """
+from aggregate_signature.lib import setup, keygen, aggregate_sign, aggregate_keys, randomize
+
+
+
+# ==================================================
+# main -- simulate the protocol
+# ==================================================
+def main():
+	params = setup()
+	signer_url = r'http://127.0.0.1:5001/sign'
+
+	# user parameters
+	m = 10
+
+	r = requests.post(signer_url, data = dumps(initSensor))
+	print(loads(r.text))
+
+	# signer 1
+	"""
+	(sk1, vk1) = keygen(params)
+	sig1 = sign(params, sk1, m)
+
+	# signer 2
+	(sk2, vk2) = keygen(params)
+	sig2 = sign(params, sk2, m)
+
+	# affregate signatures
+	sig = aggregate_sign(sig1, sig2)
+	"""
+
+	# randomize signature
+	#randomize(params, sig)
+
+	# aggregate keys
+	"""
+	vk = aggregate_keys(vk1, vk2)
+
+	# verify signature
+	assert verify(params, vk, m, sig)
+	"""
+
+
+# ==================================================
+# entry point
+# ==================================================
+if __name__ == '__main__':
+    main()
