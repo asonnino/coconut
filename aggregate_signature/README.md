b7bb693cb0c2ce12c551af2ec3f58ac9b2a2dc1b
Alberto Sonnino <alberto@asonnino2.cs.ucl.ac.uk>
2017-10-16 16:33:45 +0100

    [CLEANUP] Updated directory tree.

--- aggregate_signature/README.md
+++
@@ -1,9 +0,0 @@
-
-# petlib
-pip install petlib
-
-# bplib
-python setup.py install
-
-# testbench
-pip install numpy
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'aggregate_signautre',
+    version = '0.1',
+    packages = ['aggregate_signautre'],
+    install_requires = [
+        'petlib',
+    ],
+)
