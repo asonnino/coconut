ae1fdd1d8d82516e7d82a72270fba95509848da1
Alberto Sonnino <alberto.sonnino@icloud.com>
2017-10-24 17:23:58 +0100

    [ADD] Fix implementation.

--- aggregate_signature/aws/signer.py
+++ aggregate_signature/aws/signer.py
@@ -1,8 +1,11 @@
 """ """
 from aggregate_signature.lib import setup, keygen, sign
+from bplib.bp import G2Elem, G1Elem
 
 from json  import loads, dumps
 from flask import Flask, request
+from binascii import hexlify, unhexlify
+import sys
 
 
 
@@ -18,12 +21,33 @@ class Signer():
 		(self.sk, self.vk) = keygen(self.params)
 
 	# ----------------------------------------------
+	# pack/unpack EC point
+	# ----------------------------------------------
+	def _pack(self, elem):
+		return hexlify(elem.export()).decode()
+
+	def _unpackG1(self, elem):
+		G = self.params[0]
+		return G1Elem.from_bytes(unhexlify(elem.encode()), G)
+
+	def _unpackG2(self, elem):
+		G = self.params[0]
+		return G2Elem.from_bytes(unhexlify(elem.encode()), G)
+
+	# ----------------------------------------------
 	# sign
 	# ----------------------------------------------
 	def issue_signature(self, requestData):
-		print(requestData)
 		m = loads(requestData)
-		return sign(self.params, self.sk, m)
+		sig1, sig2 = sign(self.params, self.sk, m)
+		return (self._pack(sig1), self._pack(sig2))
+
+	# ----------------------------------------------
+	# send verification key
+	# ----------------------------------------------
+	def send_vk(self):
+		(a, b, c) = self.vk
+		return (self._pack(a), self._pack(b), self._pack(c))
 
 
 
@@ -32,14 +56,12 @@ class Signer():
 # Webapp
 # ==================================================
 app = Flask(__name__)
-app.signer = Signer()
 
 
 # index
 @app.route("/", methods=['GET', 'POST'])
 def app_index():
-    return dumps({"status": "OK", "message": "Hello, world!"})
-
+    return dumps({"status": "OK", "message": "Hello, from Signer!"})
 
 # issue a signature 
 @app.route("/sign", methods=["GET", "POST"])
@@ -49,6 +71,20 @@ def app_sign():
         	returns = app.signer.issue_signature(request.data)
         	return dumps({"status" : "OK", "returns" : returns})
         except Exception as e:
+        	print(e)
+        	return dumps({"status"  : "ERROR", "message" : e.args})
+    else:
+    	return dumps({"status": "ERROR", "message":"Use POST method."})
+
+# send verification key
+@app.route("/key", methods=["GET", "POST"])
+def app_key():
+    if request.method == "POST":
+        try:
+        	returns = app.signer.send_vk()
+        	return dumps({"status" : "OK", "returns" : returns})
+        except Exception as e:
+        	print(e)
         	return dumps({"status"  : "ERROR", "message" : e.args})
     else:
     	return dumps({"status": "ERROR", "message":"Use POST method."})
@@ -59,5 +95,7 @@ def app_sign():
 # entry point
 # ==================================================
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port="5001") 
+	port = sys.argv[1]
+	app.signer = Signer()
+	app.run(host="127.0.0.1", port=port) 
 
--- aggregate_signature/aws/user.py
+++ aggregate_signature/aws/user.py
@@ -1,46 +1,108 @@
 """ """
-from aggregate_signature.lib import setup, keygen, aggregate_sign, aggregate_keys, randomize
+from aggregate_signature.lib import setup, keygen, aggregate_sign, aggregate_keys, randomize, verify
+from bplib.bp import G2Elem, G1Elem
 
 from json  import loads, dumps
+from binascii import hexlify, unhexlify
 import requests
+
+# ==================================================
+# Constants
+# ==================================================
+SIGNER_PORT = 5000
+VERIFIER_PORT = 4000
+
+
+# ==================================================
+# User class
+# ==================================================
+class User():
+	# ----------------------------------------------
+	# init
+	# ----------------------------------------------
+	def __init__(self, signer_addr, verifier_addr):
+		self.signer_addr = signer_addr
+		self.verifier_addr = verifier_addr +':'+str(VERIFIER_PORT)
+		self.params = setup()
+
+
+	# ----------------------------------------------
+	# pack/unpack EC point
+	# ----------------------------------------------
+	def _pack(self, elem):
+		return hexlify(elem.export()).decode()
+
+	def _unpackG1(self, elem):
+		G = self.params[0]
+		return G1Elem.from_bytes(unhexlify(elem.encode()), G)
+
+	def _unpackG2(self, elem):
+		G = self.params[0]
+		return G2Elem.from_bytes(unhexlify(elem.encode()), G)
+
+	# ----------------------------------------------
+	# ask signature
+	# ----------------------------------------------
+	def _ask_signature_single(self, m, signer_url):
+		try:
+			r = requests.post(signer_url+'/sign', data = dumps(m))
+			returns = loads(r.text)['returns']
+			sig1 = self._unpackG1(returns[0])
+			sig2 = self._unpackG1(returns[1])
+			return (sig1, sig2)
+		except Exception as e:
+			print(e)
+			return None
+
+	def ask_signature(self, m, n):
+		sigs = []
+		for i in range(0,n):
+			signer_url = self.signer_addr +':'+ str(SIGNER_PORT+i)
+			sigs.append(self._ask_signature_single(m, signer_url))
+		return sigs
+
+	# ----------------------------------------------
+	# aggregate signatures
+	# ----------------------------------------------
+	def func_aggregate_sign(self, sigs):
+		sig_aggr = sigs[0]
+		for i in range(1, len(sigs)):
+			sig_aggr = aggregate_sign(sig_aggr, sigs[i])
+		return sig_aggr
+
+
+	# ----------------------------------------------
+	# ask verify
+	# ----------------------------------------------
+	def ask_verify(self, m, sig):
+		try:
+			(sig1, sig2) = sig
+			data = {'message':m, 'signature': (self._pack(sig1),self._pack(sig2))}
+			r = requests.post(self.verifier_addr+'/verify', data = dumps(data))
+			return loads(r.text)['returns'] == True
+		except Exception as e:
+			print(e)
+			return False
+
 
 
 # ==================================================
 # main -- simulate the protocol
 # ==================================================
 def main():
-	#params = setup()
-	signer_url = r'http://127.0.0.1:5001/sign'
-
-	# user parameters
+	n = 2
 	m = 10
+	signer_addr = r'http://127.0.0.1'
+	verifier_addr = r'http://127.0.0.1'
+	user = User(signer_addr, verifier_addr)
 
-	r = requests.post(signer_url, data = dumps(m))
-	print(loads(r.text))
+	# ask for signature
+	sigs = user.ask_signature(m,n)
+	sig = user.func_aggregate_sign(sigs)
 
-	# signer 1
-	"""
-	(sk1, vk1) = keygen(params)
-	sig1 = sign(params, sk1, m)
+	# ask for verification
+	print(user.ask_verify(m, sig))
 
-	# signer 2
-	(sk2, vk2) = keygen(params)
-	sig2 = sign(params, sk2, m)
-
-	# affregate signatures
-	sig = aggregate_sign(sig1, sig2)
-	"""
-
-	# randomize signature
-	#randomize(params, sig)
-
-	# aggregate keys
-	"""
-	vk = aggregate_keys(vk1, vk2)
-
-	# verify signature
-	assert verify(params, vk, m, sig)
-	"""
 
 
 # ==================================================
@@ -48,3 +110,5 @@ def main():
 # ==================================================
 if __name__ == '__main__':
     main()
+
+
