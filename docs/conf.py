5ff66b9daf49c77b12ce5f8f3db54f473d218e18
asonnino <alberto.sonnino@ucl.ac.uk>
2018-05-09 11:05:53 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,6 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-html_style = '/default.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
