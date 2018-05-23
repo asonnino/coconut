3d5dc2134504def67204db17067ebc25f4d3c272
asonnino <alberto.sonnino@ucl.ac.uk>
2018-05-23 13:51:45 +0100

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,17 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(r'..'))
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['bplib', 'bplib.bp', 'bplib.bp.BpGroup', 'bplib.bp.G2Elem']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
